a480766f50effa4f91a98178ff0fd0f1d18dd045
Hyoru <wiktor.jaskiewicz11@gmail.com>
2019-07-28 22:31:05 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,18 +3,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        
+        //User input
         Scanner input = new Scanner(System.in);
+
         System.out.println("Check if your number can be divided by 3 and 5 at the same time.");
         System.out.println("Please input your number and press enter: ");
+        
+        //Value that we will use for if/else loop
         double userInput = input.nextDouble();
 
+        // if/else loop for dividing by 3 and 5 at the same time
         if(userInput % 3 == 0 && userInput % 5 == 0){
             System.out.println("True");
         }
         else{
             System.out.println("false");
         }
-
 
     }
 }
